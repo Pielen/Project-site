974c64410cf2a83434907558a41e41741b701792
Adam Pielbusch <adampi@kth.se>
2017-12-05 16:53:17 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <div>
             <Route exact path="/" component={StartPage}/>
             <Route path="/dos" component={DOSpage}/>
